b1ef1097be52bc9960d88c4d9fbbe3dad6a64658
Jasiukiewicz Tymon <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-12-20 22:25:39 +0100

    Update init.rs

--- test/init.rs
+++ test/init.rs
@@ -1,9 +1,10 @@
 use std::env;
 
 mod command;
+mod stylus;
 
 fn getcommand(args: Vec<String>) {
-
+    println!("{:?}", &args)
 }
 
 fn main() {
@@ -12,16 +13,22 @@ fn main() {
             name: String::from("get"),
             help: String::from("get <url> <selector>"),
             details: String::from("..."),
+            argslen: vec![4],
+            argserror: String::from("ARG ERROR"),
             run: getcommand
         },
         command::Command {
             name: String::from("observe"),
             help: String::from("observe <url> <selector> <timeout>"),
             details: String::from("..."),
+            argslen: vec![],
+            argserror: String::from("ARG ERROR"),
             run: getcommand
         }
     ];
 
+    stylus::styledprint("test", "color: red", true);
+
     let args: Vec<String> = env::args().collect();
-    command::mount(commands, args);
+    command::mount(commands, args, "error: This command doesn't exists".to_string());
 }
